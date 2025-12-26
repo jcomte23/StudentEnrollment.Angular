0ff9eb5871225da466be1363cb0b33faa5ecc122
Javier Combita Tellez <javiercombita2014@gmail.com>
2025-12-26 17:55:11 -0500

    feat(ui): add visual components to consume Angular app
    Create the necessary UI components to interact with our application,
    including main views, buttons, and layouts, ready to integrate with backend services.

---
+++ app/src/app/features/courses/course-students/course-students.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-students',
+  imports: [],
+  templateUrl: './course-students.html',
+  styleUrl: './course-students.css',
+})
+export class CourseStudents {
+
+}
